27b9e7ed024969618b7654b38245be5ca1c9cc64
chrisvidell23 <Christophervl.johnson@gmail.com>
2017-03-24 10:54:35 -0400

    Add project files.

---
+++ AutoDealership/Controllers/AutomobileController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoDealership.Models;
+
+namespace AutoDealership.Controllers
+{
+    public class AutomobileController : Controller
+    {
+        // GET: Automobile
+        public ActionResult Index() 
+        {
+            var automobiles = from a in GetAutomobileList()
+                              orderby a.ID
+                              select a;
+            return View(automobiles);
+        }
+
+        // GET: Automobile/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Automobile/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Automobile/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Automobile/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Automobile/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Automobile/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Automobile/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [NonAction]
+        public List<Automobile> GetAutomobileList()
+        {
+            return new List<Automobile>
+            {
+                new Automobile
+                {
+                    ID = 1,
+                    Make = "Mercedes Benz",
+                    Model = "S550",
+                    Year = DateTime.Parse(DateTime.Today.ToString()),
+                    Color = "Blue",
+                },
+
+                new Automobile
+                {
+                    ID =2,
+                    Make = "Lexus",
+                    Model = "GS530",
+                    Year = DateTime.Parse(DateTime.Today.ToString()),
+                    Color = "White",
+                },
+
+                new Automobile
+                {
+                    ID =3,
+                    Make = "BMW",
+                    Model = "Li750",
+                    Year = DateTime.Parse(DateTime.Today.ToString()),
+                    Color = "Silver",
+                },
+
+                new Automobile
+                {
+                    ID= 4,
+                    Make= "Chrysler",
+                    Model= "Avenger",
+                    Year= DateTime.Parse(DateTime.Today.ToString()),
+                    Color = "Black",
+                    
+                },
+            };
+        }
+    }
+
+}
---
+++ AutoDealership/Models/Automobile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoDealership.Models;
+
+namespace AutoDealership.Models
+{
+    public class Automobile
+    {
+        public int ID { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public DateTime Year { get; set; }
+        public string Color { get; set; }
+    }
+}
